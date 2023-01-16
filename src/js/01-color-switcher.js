060de7ce8809ef0de7580c213003a9cffdfd678a
PokoenkoS <pokoenkosveta@gmail.com>
2023-01-16 13:45:08 +0200

    add a task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const refs ={
+    start: document.querySelector(`[data-start]`),
+    stop: document.querySelector(`[data-stop]`)
+  
+}
+refs.start.addEventListener(`click`, onStartChangeColors);
+refs.stop.addEventListener(`click`, onStopChangeColors);
+let timeOutId = null;
+refs.start.disabled = false
 
+function onStartChangeColors(evt) {
+    if (refs.start.disabled){
+return;
+    }
+  timeOutId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();}, 1000
+    )
+    refs.start.disabled = true;
+
+}
+
+function onStopChangeColors() {
+    clearInterval(timeOutId);
+    refs.start.disabled = false
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
