7539e927ef687d1736767638b13624273e5e68b2
PokoenkoS <pokoenkosveta@gmail.com>
2023-01-17 09:23:50 +0200

    add a task 2 onClose

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,36 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+const btn = document.querySelector(`[data-start]`);
+btn.disabled = true;
+const input = document.getElementById(`datetime-picker`);
+
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+       
+        if (selectedDates[0] < options.defaultDate ){
+            window.alert("Please choose a date in the future");
+            btn.disabled = true;
+        } else {
+           
+            btn.disabled = false;
+            
+        }
+     
+    },
+  }
+  flatpickr(input, options, );
+
+  
+
+ 
+  
+
+  
+  
+    
+  
